82a6424a0df20d5db75092f7839b85c8d6ae4fab
Yosuke Inoue <ysk.inoue@sharesl.net>
2023-10-20 11:07:28 +0900

    [fix] CSS・JSバンドル

--- dist/js/disable-back-button.js
+++ dist/js/disable-back-button.js
@@ -1,15 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!***************************************!*\
-  !*** ./src/js/disable-back-button.js ***!
-  \***************************************/
-(() => {
-  window.history.pushState(null, null, document.URL);
-  window.addEventListener("popstate", function() {
-    window.history.pushState(null, null, document.URL);
-  });
-})();
-
-/******/ })()
-;
+window.history.pushState(null,null,document.URL),window.addEventListener("popstate",(function(){window.history.pushState(null,null,document.URL)}));
 //# sourceMappingURL=disable-back-button.js.map
