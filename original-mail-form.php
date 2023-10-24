ba63a6418c8dc3c9666ad06913613f633d2feb91
Yosuke Inoue <ysk.inoue@sharesl.net>
2023-10-25 01:30:17 +0900

    [fix] セッションハイジャック対策

--- original-mail-form.php
+++ original-mail-form.php
@@ -92,11 +92,11 @@ class Original_Mail_Forms
    */
   public function init_sessions()
   {
+    ini_set('session.use_cookies', '1');
+    ini_set('session.use_only_cookies', '1');
+    ini_set('session.cookie_secure', '1');
+    ini_set('session.cookie_httponly', '1');
     if (!$this->is_rest() && session_status() !== PHP_SESSION_ACTIVE) {
-      ini_set('session.use_cookies', '1');
-      ini_set('session.use_only_cookies', '1');
-      ini_set('session.cookie_secure', '1');
-      ini_set('session.cookie_httponly', '1');
       session_start();
       header('Expires:-1');
       header('Cache-Control:');
